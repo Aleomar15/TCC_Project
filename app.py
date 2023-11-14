039202b89eead67488cda661795da82850862877
Alexandre O Marinho <alexandreomar0202@gmail.com>
2023-11-14 10:58:11 -0300

    hespedando

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 from flask import Flask, request, render_template
 import pickle
 
@@ -20,4 +21,5 @@ def predict():
     return render_template('index.html', prediction_text='Quantidade de casos previstas é{}'.format(prediction))
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
